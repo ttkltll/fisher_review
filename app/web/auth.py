63d663281a2a229556d438f2e7093e8b4c752293
ttkltll <ttkltll@163.com>
2020-07-23 12:11:06 +0800

    重构login,让它可以跳转到之前页面

--- app/web/auth.py
+++ app/web/auth.py
@@ -14,7 +14,7 @@ def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
         user = User()
-        user.set_attrs(form.data) # 拿到表格的数据，把它们写到数据库里
+        user.set_attrs(form.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('web.login'))
@@ -27,13 +27,17 @@ def login():
     1检验数据是否合法
     2检验数据邮箱这个用户是否存在，存在的话，密码是否对得上，这个都可以用第三方Form来验证
     3写入cookie,到对方，对方上次把Id带来，和session中的一对，发现数据库中如果有的话，就给对方返回登录后才会显示的页面，比如首面右上角的"注销',
+    4重构login,它除了能返回主页，还能返回到之前的跳转的页面
     :return:
     '''
     form = LoginForm(request.form)
     if request.method == 'POST' and form.validate():
         user = User.query.filter_by(email=form.email.data).first()
         if user and user.check_password(form.password.data):
-            login_user(user, remember=True)  # 直接拿对象，把对象的写到current_app中，
+            login_user(user, remember=True)
+            if request.args:
+                next = request.args['next']
+                redirect(next)
             return render_template('index.html')
         else:
             flash('账号不存在或密码错误')
