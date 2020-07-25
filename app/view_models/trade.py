555853bede7405ca19d4fe16dd5694804c1ff3a0
ttkltll <ttkltll@163.com>
2020-07-25 15:00:12 +0800

    !!!,这里用嵌套循环，不用列表推导式。

--- app/view_models/trade.py
+++ app/view_models/trade.py
@@ -17,23 +17,42 @@ class TradeInfo:
         self.trades = [self.__map_to_trade(single) for single in goods]
 
     def __map_to_trade(self, single):
+        """
         if single.create_datetime:
             time = single.create_datetime.strftime('%Y-%m-%d')
         else:
             time = '未知'
+         """
         return dict(
             user_name=single.user.nickname,
-            time=time,
             id=single.id
         )
 
-class MyTrades():
-    def __init__(self, lists_mine, count_list):
+
+class MyTrades:
+    def __init__(self, trades_of_mine, trade_count_list):
         self.trades = []
-        self._map(lists_mine, count_list )
 
-    def _map(self, lists_mine, count_list ):
-        self.trades = [for x in lists_mine]
-        # 写到这，我不知道怎么写，这里看似要用两个列表推导式组合成一个。
+        self.__trades_of_mine = trades_of_mine
+        self.__trade_count_list = trade_count_list
 
+        self.trades = self.__parse()
 
+    def __parse(self):
+        temp_trades = []
+        for trade in self.__trades_of_mine:
+            my_trade = self.__matching(trade)
+            temp_trades.append(my_trade)
+        return temp_trades
+
+    def __matching(self, trade):
+        count = 0
+        for trade_count in self.__trade_count_list:
+            if trade.isbn == trade_count['isbn']:
+                count = trade_count['count']
+        r = {
+            'wishes_count': count,
+            'book': BookViewModel(trade.book),
+            'id': trade.id
+        }
+        return r
